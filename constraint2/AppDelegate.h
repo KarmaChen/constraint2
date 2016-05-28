03477c82de40689003e3f7a1ca4553d3115adfe1
KarmaChen <229192590@qq.com>
2016-05-28 22:31:33 +0800

    first

---
+++ constraint2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  constraint2
+//
+//  Created by Karma on 16/4/24.
+//  Copyright © 2016年 陈昆涛. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ constraint2/ViewController.m
@@ -0,0 +1,35 @@
+//
+//  ViewController.m
+//  constraint2
+//
+//  Created by Karma on 16/4/24.
+//  Copyright © 2016年 陈昆涛. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)buttonTapped:(UIButton *)sender
+{
+    if ([[sender titleForState:UIControlStateNormal] isEqualToString:@"X"]) {
+        [sender setTitle:@"A very long title for this button"
+                forState:UIControlStateNormal];
+    } else {
+        [sender setTitle:@"X" forState:UIControlStateNormal];
+    }
+}
+@end
